d3e9eed1ecb2451e15f7968d9467f391b861e9de
Gaurav <katariagaurav686@gmail.com>
2025-05-18 17:44:35 +0530

    feat:added prisma schema for video status

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -7,9 +7,12 @@ const express_1 = __importDefault(require("express"));
 const Server_config_1 = require("./config/Server.config");
 const Routes_1 = __importDefault(require("./Routes"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use('/api', Routes_1.default);
+console.log(path_1.default.join(__dirname, '../output'));
+app.use('/output', express_1.default.static(path_1.default.join(__dirname, '../output')));
 app.listen(Server_config_1.PORT, () => {
     console.log(`server started at ${Server_config_1.PORT}`);
 });
